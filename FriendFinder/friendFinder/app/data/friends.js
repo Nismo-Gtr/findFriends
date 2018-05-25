1ca01fd8d024d427fc6a0e05b7ad12fda69c194c
Nismo-Gtr <NCowan3@outlook.com>
2018-05-24 22:31:51 -0500

    friend finder

---
+++ FriendFinder/friendFinder/app/data/friends.js
@@ -0,0 +1,70 @@
+var friends = [
+    {
+    name: "Nosferatu",
+    photo: 'nosferatu.jpg',
+    scores:[
+        1,
+        2,
+        3,
+        4,
+        5,
+        4,
+        3,
+        2,
+        1,
+        2,
+    ]
+},
+{
+    name: "Blade",
+    photo: '',
+    scores:[
+        1,
+        2,
+        3,
+        4,
+        5,
+        4,
+        3,
+        2,
+        1,
+        2,
+    ]
+},
+{
+    name: "Harry from Harry and the Hendersons",
+    photo: '',
+    scores:[
+        1,
+        2,
+        3,
+        4,
+        5,
+        4,
+        3,
+        2,
+        1,
+        2,
+    ]
+},
+{
+    name: "Greg Kinnear",
+    photo: '',
+    scores:[
+        5,
+        5,
+        3,
+        5,
+        5,
+        4,
+        3,
+        2,
+        1,
+        2,
+    ]
+},
+
+]
+
+module.exports = friends;
+
---
+++ FriendFinder/friendFinder/server.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// app.get("./routes/apiRoutes", function (req, res){
+//     console.log(res);
+// })
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
